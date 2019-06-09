d3605cf38a5406df796c3223e5753a9259229266
Max Lee <maxolee23@gmail.com>
2019-06-09 17:35:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,30 @@
 function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+  for (let i = 0; i < 25; i++) {
+    if(i === 1) {
+      array.push(`I am ${i} strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
   }
   return array;
 }
 
 function whileLoop(n) {
-  let countdown = n;
+  while(n > 0){
+    console.log(n--);
+ }
+  return 'done';
+}
 
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
-  }
+function doWhileLoop(num) {
+  var i = 0;
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+}
 
   do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+    console.log('I run once regardless.');
+  } while(incrementVariable() < num);
 }
